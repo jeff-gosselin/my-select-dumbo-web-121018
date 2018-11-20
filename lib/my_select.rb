f7c6a547d1d22e9ada312d48cbe2c6d928ae8b13
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-20 18:34:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+ i = 0 
+ new_array = []
+ while i < collection.length 
+    new_array.push(yield collection[i]) if true
+  i += 1 
+ end
+ new_array
 end
